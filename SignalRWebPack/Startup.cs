ead23a0677ff8a592afd9dec06ce50bb9d469992
rinoy <rinoy@promactinfo.com>
2019-11-18 11:46:28 +0530

    broadcast chat working

--- SignalRWebPack/Startup.cs
+++ SignalRWebPack/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using SignalRWebPack.Hubs;
+
 
 namespace SignalRWebPack
 {
@@ -15,6 +17,7 @@ namespace SignalRWebPack
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +28,12 @@ namespace SignalRWebPack
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseSignalR(options =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                options.MapHub<ChatHub>("/hub");
             });
         }
     }
